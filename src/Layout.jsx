1ed3e43853bc27f392d070281985ef4e8a213dda
Benjamin Weis <benweis@wisemediaads.com>
2026-02-07 15:44:37 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -38,7 +38,7 @@ export default function Layout({ children, currentPageName }) {
 
   useEffect(() => {
     setMobileMenuOpen(false);
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
   }, [location]);
 
   const isHomePage = currentPageName === 'Home';
